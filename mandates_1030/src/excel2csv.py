45ef371f51269e857819145c1463bde74ba52c02
fdurant <frederik.durant@pandora.be>
2017-06-16 15:33:31 +0200

    Now also writes hash IDs for people and orgs

--- mandates_1030/src/excel2csv.py
+++ mandates_1030/src/excel2csv.py
@@ -4,6 +4,7 @@ import sys
 from xlrd import open_workbook
 import re
 import codecs
+import hashlib
 
 def init():
     parser = argparse.ArgumentParser()
@@ -43,7 +44,7 @@ def process_file():
     mandateFH = open(args['outfile_mandates'], 'wb')
     mandateWriter = csv.writer(mandateFH, delimiter='\t')
 
-    mandateHeaders = ['org','person','role']
+    mandateHeaders = ['org','person','role','source','target']
     mandateWriter.writerow(mandateHeaders)
 
     for rowNr in range(firstRowsToIgnore,sheet.nrows):
@@ -90,15 +91,27 @@ def process_file():
 #                print >> sys.stderr, "currentOrg = %s" % currentOrg
 #                print >> sys.stderr, "person = %s" % person
 #                print >> sys.stderr, "currentHeaders[0] = %s" % currentHeaders[0]
-                mandateWriter.writerow([currentOrg.encode('utf-8'), person.encode('utf-8'), currentHeaders[0].encode('utf-8')])
+                mandateWriter.writerow([currentOrg.encode('utf-8'), 
+                                        person.encode('utf-8'), 
+                                        currentHeaders[0].encode('utf-8'),
+                                        hashlib.sha1(person.encode("UTF-8")).hexdigest(),
+                                        hashlib.sha1(currentOrg.encode("UTF-8")).hexdigest()])
             if cells[1] != '':
                 person, address = parse_person_address(cells[1])
                 person2address[person] = address
-                mandateWriter.writerow([currentOrg.encode('utf-8'), person.encode('utf-8'), currentHeaders[1].encode('utf-8')])
+                mandateWriter.writerow([currentOrg.encode('utf-8'), 
+                                        person.encode('utf-8'), 
+                                        currentHeaders[1].encode('utf-8'),
+                                        hashlib.sha1(person.encode("UTF-8")).hexdigest(),
+                                        hashlib.sha1(currentOrg.encode("UTF-8")).hexdigest()])
             if cells[2] != '':
                 person, address = parse_person_address(cells[2])
                 person2address[person] = address
-                mandateWriter.writerow([currentOrg.encode('utf-8'), person.encode('utf-8'), currentHeaders[2].encode('utf-8')])
+                mandateWriter.writerow([currentOrg.encode('utf-8'), 
+                                        person.encode('utf-8'), 
+                                        currentHeaders[2].encode('utf-8'),
+                                        hashlib.sha1(person.encode("UTF-8")).hexdigest(),
+                                        hashlib.sha1(currentOrg.encode("UTF-8")).hexdigest()])
 
     print >> sys.stderr, "Wrote %s" % args['outfile_mandates']
 
@@ -107,11 +120,13 @@ def process_file():
     peopleFH = open(args['outfile_people'], 'wb')
     peopleWriter = csv.writer(peopleFH, delimiter='\t')
 
-    peopleHeaders = ['person','address']
+    peopleHeaders = ['person','address','id']
     peopleWriter.writerow(peopleHeaders)
 
     for person in sorted(person2address.keys()):
-        peopleWriter.writerow([person.encode('utf-8'), person2address[person].encode('utf-8')])
+        peopleWriter.writerow([person.encode('utf-8'), 
+                               person2address[person].encode('utf-8'),
+                               hashlib.sha1(person.encode("UTF-8")).hexdigest()])
 
     print >> sys.stderr, "Wrote %s" % args['outfile_people']
 
@@ -120,11 +135,13 @@ def process_file():
     orgFH = open(args['outfile_org'], 'wb')
     orgWriter = csv.writer(orgFH, delimiter='\t')
 
-    orgHeaders = ['org','address']
+    orgHeaders = ['org','address','id']
     orgWriter.writerow(orgHeaders)
 
     for org in sorted(organization2address.keys()):
-        orgWriter.writerow([org.encode('utf-8'), organization2address[org].encode('utf-8')])
+        orgWriter.writerow([org.encode('utf-8'),
+                            organization2address[org].encode('utf-8'),
+                            hashlib.sha1(org.encode("UTF-8")).hexdigest()])
 
     print >> sys.stderr, "Wrote %s" % args['outfile_org']
 
